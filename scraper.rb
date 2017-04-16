8c1d4834fac0be67870bb6e3d5aa4fef0fd193f6
Nikhil Lele <nik.lele@gmail.com>
2017-04-16 14:27:10 -0700

    split up details into separate vars

--- scraper.rb
+++ scraper.rb
@@ -30,68 +30,78 @@ def monsterPages()
     return pages
 end
 
+def textBlockHash(monster, titleXPath, descriptionXPath)
+    titles = monster.xpath(titleXPath).map { |e| e.text }
+    descriptions = monster.xpath(descriptionXPath).map { |e| e.text }
+    data = Hash[[titles, descriptions].transpose]
+    data.default = ""
+    return data
+end
+
 def textBlock(monster, titleXPath, descriptionXPath)
     titles = monster.xpath(titleXPath).map { |e| e.text }
-    descriptions =  monster.xpath(descriptionXPath).map { |e| e.text }
+    descriptions = monster.xpath(descriptionXPath).map { |e| e.text }
     return titles.zip(descriptions).join(" ")
 end
 
 def monsterPage(url)
+    data = Hash.new("")
     monsterHtml = HTTParty.get(url)
     monster = Nokogiri::HTML(monsterHtml).xpath('//*[@id="app"]/div/div[3]')
 
-    title = monster.xpath('//h1/span').text
-    puts "title: #{title}"
+    data['title'] = monster.xpath('//h1/span').text
 
     info = monster.xpath('//*[@id="app"]/div/div[3]/div/div/div/div[1]')
 
-    size = info.xpath('//div[1]/em[1]').text
-    type = info.xpath('//div[1]/em[2]').text
-    alignment = info.xpath('//div[1]/em[4]').text
-    ac = info.xpath('//div[2]/div[1]/span').text
-    speed = info.xpath('//div[2]/div[3]/span').text
-    puts "size: #{size} type: #{type} alignment: #{alignment} ac: #{ac} speed: #{speed}"
+    data['size'] = info.xpath('//div[1]/em[1]').text
+    data['type'] = info.xpath('//div[1]/em[2]').text
+    data['alignment'] = info.xpath('//div[1]/em[4]').text
+    data['ac'] = info.xpath('//div[2]/div[1]/span').text
+    data['speed'] = info.xpath('//div[2]/div[3]/span').text
 
-    hp_avg = info.xpath('//div[2]/div[2]/span/span/span[1]').text
-    hp_dice = info.xpath('//div[2]/div[2]/span').text.split(/[()]/)[1].delete(' ')
-    puts "hp_avg: #{hp_avg} hp_dice: #{hp_dice}"
+    data['hp_avg'] = info.xpath('//div[2]/div[2]/span/span/span[1]').text
+    data['hp_dice'] = info.xpath('//div[2]/div[2]/span').text.split(/[()]/)[1].delete(' ')
 
     stats = info.xpath('//div[3]/table/tbody/tr')
-    str = stats.xpath('//td[1]/div').text
-    dex = stats.xpath('//td[2]/div').text
-    con = stats.xpath('//td[3]/div').text
-    int = stats.xpath('//td[4]/div').text
-    wis = stats.xpath('//td[5]/div').text
-    cha = stats.xpath('//td[6]/div').text
-    puts "str: #{str} dex: #{dex} con: #{con} int: #{int} wis: #{wis} cha: #{cha}"
+    data['str'] = stats.xpath('//td[1]/div').text
+    data['dex'] = stats.xpath('//td[2]/div').text
+    data['con'] = stats.xpath('//td[3]/div').text
+    data['int'] = stats.xpath('//td[4]/div').text
+    data['wis'] = stats.xpath('//td[5]/div').text
+    data['cha'] = stats.xpath('//td[6]/div').text
 
-    # TODO spit details into each category if it exists:
-    # proficiency, saving_throws, skills, senses, languages, cr, xp
     detailTitleXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[1]/div[4]/div/h5'
     detailDescriptionXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[1]/div[4]/div/span'
-    details = textBlock(monster, detailTitleXPath, detailDescriptionXPath)
-    puts "details: #{details}"
+    details = textBlockHash(monster, detailTitleXPath, detailDescriptionXPath)
+    data['proficiency'] = details['Proficiency Bonus']
+    data['saving_throws'] = details['Saving Throws']
+    data['skills'] = details['Skills']
+    data['senses'] = details['Senses']
+    data['languages'] = details['Languages']
+    data['cr'] = details['Challenge'].split(/[() ]/)[0]
+    data['xp'] = details['Challenge'].split(/[() ]/)[1]
 
+    # TODO figure out how to split up special, actions, and legendary actions
+    # eg. to support looking for a specific action
     specialTitleXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[1]/div[5]/p/em/strong'
     specialDescriptionXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[1]/div[5]/p/span'
-    special = textBlock(monster, specialTitleXPath, specialDescriptionXPath)
-    puts "special: #{special}"
+    data['special'] = textBlock(monster, specialTitleXPath, specialDescriptionXPath)
 
     actionsTitleXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[3]/div/div[2]/p/em/strong'
     actionsDescriptionXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[3]/div/div[2]/p/span'
-    actions = textBlock(monster, actionsTitleXPath, actionsDescriptionXPath)
-    puts "actions: #{actions}"
+    data['actions'] = textBlock(monster, actionsTitleXPath, actionsDescriptionXPath)
 
-    legendaryActionsText = monster.xpath('//*[@id="app"]/div/div[3]/div/div/div/div[3]/div/div[3]/p[1]').text
+    data['legendaryActionsText'] = monster.xpath('//*[@id="app"]/div/div[3]/div/div/div/div[3]/div/div[3]/p[1]').text
     legendaryActionsTitleXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[3]/div/div[3]/p/em/strong'
     legendaryActionsDescriptionXPath = '//*[@id="app"]/div/div[3]/div/div/div/div[3]/div/div[3]/p/span'
-    legendaryActions = textBlock(monster, legendaryActionsTitleXPath, legendaryActionsDescriptionXPath)
-    puts "legendaryActionsText: #{legendaryActionsText} legendaryActions: #{legendaryActions}"
+    data['legendaryActions'] = textBlock(monster, legendaryActionsTitleXPath, legendaryActionsDescriptionXPath)
+
+    puts data
 
 end
 
 monsterPage('http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters/hobgoblin')
 
-# monsterPage('http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters/aboleth')
+monsterPage('http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters/aboleth')
 
 # monsterPage('http://www.orcpub.com/dungeons-and-dragons/5th-edition/monsters/gnome--deep-svirfneblin')
